4d41450897a086e217c9a635bb4e246eba27e3f2
Sawyer Labanowski <labanowskisawyer@gmail.com>
2019-05-04 19:52:11 -0500

    Added thrust, rotation, and audio functionality. Organized files.

--- Project_Boost/Assets/Scripts/Rocket.cs
+++ Project_Boost/Assets/Scripts/Rocket.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audio;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,15 +26,25 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+
+            if (!audio.isPlaying)
+            {
+                audio.Play();
+            }                
         }
+        else
+        {
+            audio.Stop();
+        }
+
 
         if (Input.GetKey(KeyCode.A))
         {
-
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
